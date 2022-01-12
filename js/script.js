589be5c31cedcdf64b117d1f2201ec0a364da136
David Machado Monteiro <2211849@my.ipleiria.pt>
2022-01-12 20:29:42 +0000

    Struct correction

--- js/script.js
+++ js/script.js
@@ -4,34 +4,30 @@
 var API_url = "https://api.coingecko.com/api/v3/";
 var get_ping = "ping";
 var get_coins = "coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false";
-var cloneMedia = $('.media').clone();
+var cloneMedia = $('.media');
+var page_name = window.location.pathname.split("/").slice(-1);
 
-class moeda{
-    constructor(rank, nome, valor, fav) {
-        this.rank = rank;
-        this.nome = nome;
-        this.valor = valor;
-        this.fav = fav;
-    }
-};
-
-$(document).ready(function() {
-	$.ajax({
-		method: "GET",
-		url: API_url + get_coins
-	}).done(function(res){
-		console.log(res);
-
-		$.each(res, function(index, result){
-			// Criar novo clone da lista
-			var liMedia = cloneMedia.clone();
-			// Alterar campos do item
-			$('#rank', liMedia).text(result.market_cap_rank);
-			$('#image', liMedia).attr('src', result.image);
-			$('#nome', liMedia).text(result.name);
-			$('#valor', liMedia).text(result.Title);
-			// Adicionar o clone à tabela original
-			$('.media-list').append(liMedia);
+$(window).on('load',function() {
+	console.log(page_name)
+	if(page_name == 'test.html'){
+		$.ajax({
+			method: "GET",
+			url: API_url + get_coins
+		}).done(function(res){
+			console.log(res);
+			$.each(res, function(index, result){
+				// Criar novo clone da lista
+				var liMedia = cloneMedia.clone();
+				// Alterar campos do item
+				$('.rank', liMedia).text(result.market_cap_rank)
+				$('#image', liMedia).attr('src', result.image);
+				$('.nome', liMedia).text(result.name)
+				$('.valor', liMedia).text(result.Title)
+				// Adicionar o clone à tabela original
+				$('ul.media-list').append(liMedia);
+			})
 		})
-	})
+	}else if(page_name == 'favoritos.html'){
+
+	}
 });
