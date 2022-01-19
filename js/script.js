e79fec0c2fe9923ae12c178d3ad9de43b7f38e97
David Machado Monteiro <2211849@my.ipleiria.pt>
2022-01-19 20:21:05 +0000

    Cripto Pages Load
    - Letting user load new pages from API
    - Optimisação de código

--- js/script.js
+++ js/script.js
@@ -1,37 +1,28 @@
 'use strict';
 
+/*------Variaveis Gerais------*/
+var coin_type = "€";
+var current_page = 1;
+var current_country = "eur"
+var original_link_section = $('.link_section').clone();
+var fav_list = new Array();
+//fav_list = JSON.parse(localStorage.getItem('fav_list'));
+
+/*------Variaveis API------*/
 var API_url = "https://api.coingecko.com/api/v3/";
-var get_ping = "ping";
-var get_coins = "coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 var serach_coins = "search?query=";
+
+/*------Variaveis HTML------*/
 var page_name = window.location.pathname.split("/").slice(-1);
 var cloneMedia = $('.media').clone();
-var fav_list = localStorage.getItem(fav_list);
+var linha_fav = $('.fav_line').clone();
 
+/*------Função on load do página------*/
 $(window).on('load', function () {
-	if (page_name == 'test.html' || page_name == 'index.html') {
-		$.ajax({
-			method: "GET",
-			url: API_url + get_coins
-		}).done(function (res) {
-			console.log(res);
-			$.each(res, function (index, result) {
-				// Criar novo clone da lista
-				//TODO Crear estructura HTML da linha
-				var liMedia = cloneMedia.clone();
-				// Alterar campos do item
-				liMedia.id = result.id;
-				$('.rank', liMedia).text("#" + result.market_cap_rank);
-				$('.detalhes', liMedia).prop('id', result.id);
-				$('#image', liMedia).attr('src', result.image);
-				$('.nome', liMedia).text(result.name);
-				$('.valor', liMedia).text(result.current_price);
-				$('.change24h', liMedia).text(result.price_change_24h);
-				// Adicionar o clone à tabela original
-				$('.media-list').append(liMedia);
-			})
-			$('.media').first().remove();
-		})
+	reload_fav_list();
+	if (page_name == 'index.html') {
+		load_index();		
+		$('.media').first().remove();
 	} else if (page_name == 'favoritos.html') {
 
 	} else if (page_name == 'search.html') {
@@ -57,58 +48,153 @@ $(window).on('load', function () {
 	} else if (page_name == 'detalhes.html') {
 		$.ajax({
 			method: "GET",
-			url: API_url + "coins/" + "ethereum"//localStorage.getItem("coin_detail")
+			url: API_url + "coins/" + "bitcoin"//"ethereum"//localStorage.getItem("coin_detail")
 		}).done(function (res) {
 			// Criar novo clone da lista
 			$('#image').attr('src', res.image.large);
+			$('#image').attr('alt', "Logo " + res.name);
+			$('#image').attr('title', 'Logo ' + res.name);
 			$('#nome').text(res.name);
 			$('#symbol').text(res.symbol);
 			//TODO Criar função para ver se esta em lista de favoritos
 			//
 			$('#rank').text("Rank #" + res.market_cap_rank);
-			$('#price').text("€" + res.market_data.current_price.eur);
+			$('#price').text(coin_type + res.market_data.current_price.eur);
 			$('#details').html(res.description.en);
-			var original_section = $('.link_section').clone;
 			//Insert external links
-			//TODO Create link section
-
 			$.each(res.links, function (index, link_list) {
-				var section = original_section.clone;
+				var section = original_link_section.clone();
 				index = index.charAt(0).toUpperCase() + index.slice(1);
 				index = index.replace("_url", "").replace("_", " ");
-				console.log(index);
 				$(".title_link", section).text(index);
 				if (Array.isArray(link_list)) {
 					$.each(link_list, function (index, url) {
 						if (url != "" && typeof url === 'string') {
-							console.log(url)
-							/*var link_text = $(".link_text", section).clone;
-							link_text.attr("href", url);
-							link_text.text(url);
-							section.append(link_text);*/
+							var link_text = index == 0 ? $(".link_text", section) : $(".link_text", section).clone();
+							$('a', link_text).attr("href", url);
+							$('a', link_text).text(url);
+							$("ul", section).append(link_text);
 						}
 					})
-				} else if (typeof link_list === 'string' && link_list.includes("http")) {
-					var link_text = $(".link_text", section).clone;
-					link_text.attr("href", link_list);
-					link_text.text(link_list);
-					section.append(link_text);
+				} else if (typeof link_list === 'string') {
+					var link_text = $(".link_text", section);
+					if (link_list.includes("http")) {
+						$('a', link_text).attr('href', link_list);
+					}
+					$('a', link_text).text(link_list);
+
 				}
-				$("#list-links").append(section);
+				if ($("li a", section).last().text() != "") {
+					$("#list-links").append(section);
+				}
 			})
+			$('.link_section').first().remove();
 		})
 	}
 });
 
+/*------Funções especificas de objetos------*/
+
+//Guarda a info que quere ser pasada para a página de detalhes de forma local
+//e é carregada mais tarde para fazer a pesquisa uma vez que a página carrega. 
 $('#btSearch').on('click', function () {
 	var valuePesquisa = $('#pesquisa').val();
 	localStorage.setItem("coin_search", valuePesquisa);
 	window.location.href = "search.html";
 })
 
-$('.detalhes').click(function () {
+$('.detalhes').on('click', function () {
+	console.log("Im trying to get into Coin " + this.id + "details");
 	var id_coin = this.id;
-	console.log("Id I have:" + id_coin);
 	localStorage.setItem('coin_detail', id_coin);
-	//window.location.href = "detalhes.html"
+	window.location.href = "detalhes.html"
 })
+
+//-Sistema de troca de página de criptomoedas-----//
+$('.back_page').on("click", function () {
+	if (current_page > 1) {
+		$('.current_page').text("Pág." + --current_page);
+		if (current_page == 1) {
+			$('.back_page').text('');
+		}
+		removeAllChildNodes(document.querySelector('.media-list'));
+		load_index();
+	}
+})
+
+$('.next_page').on("click", function () {
+	if (current_page < 2) {
+		$('.back_page').text('<');
+	}
+	$('.current_page').text("Pág." + ++current_page);
+	removeAllChildNodes(document.querySelector('.media-list'));
+	load_index();
+})
+//---------------------------------------------------//
+
+$('#btTest').on('click', function () {
+	console.log("Im trying to load fav_list");
+	removeAllChildNodes(document.querySelector('.fav_list'));
+	reload_fav_list();
+})
+
+
+/*------Funções gerais------*/
+function removeAllChildNodes(parent) {
+	while (parent.firstChild) {
+		parent.removeChild(parent.firstChild);
+	}
+}
+
+/*------Funções Especificas------*/
+
+function reload_fav_list() {
+	var fav_table = $('.fav_list')
+	fav_table.innerHTML = '';
+	$.each(fav_list, function (index, coin) {
+		var fav_line = $(".fav_line", fav_table).clone();
+		$(".rank", fav_line).text("#" + coin.market_cap_rank);
+		$("#image", fav_line).attr('src', coin.image);
+		$(".nome", fav_line).text(coin.name);
+		$(".value", fav_line).text(coin_type + coin.current_price);
+		fav_table.append(fav_line);
+	})
+}
+
+function load_index() {
+	$.ajax({
+		method: "GET",
+		url: API_url + "coins/markets?vs_currency=" + current_country +
+			"&order=market_cap_desc&per_page=100&page=" + current_page +
+			"&sparkline=false"
+	}).done(function (res) {
+		console.log(res);
+		$.each(res, function (index, result) {
+			// Criar novo clone da lista
+			var liMedia = cloneMedia.clone();
+			// Alterar campos do item
+			liMedia.id = result.id;
+			$('.rank', liMedia).text("#" + result.market_cap_rank);
+			$('.detalhes', liMedia).prop('id', result.id);
+			$('#image', liMedia).attr('src', result.image);
+			$('.nome', liMedia).text(result.name);
+			$('.valor', liMedia).text(coin_type + result.current_price);
+			$('.change24h', liMedia).text(result.price_change_24h);
+			(result.price_change_24h > 0 ? 
+				$('.change24h', liMedia).addClass("up") : $('.change24h', liMedia).addClass("down"));
+			$('#fav_button', liMedia).checked = fav_list.includes(result);
+			$('#fav_button', liMedia).change(function () {
+				if (this.checked) {
+					fav_list.push(result);
+					console.log(fav_list);
+				} else {
+					fav_list = fav_list.filter(coin => coin.id !== result.id);
+					console.log(fav_list);
+				}
+				localStorage.setItem('fav_list', JSON.stringify(fav_list));
+			});
+			// Adicionar o clone à tabela original
+			$('.media-list').append(liMedia);
+		})
+	})
+}
