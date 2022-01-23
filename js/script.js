53f19d9e3a8eed640c5dc73e7decee5dcc5c8127
David Machado Monteiro <2211849@my.ipleiria.pt>
2022-01-23 16:48:45 +0000

    Corrections
    - Comentação de código
    - Correção de responsividade
    - Correção Web Tree

--- js/script.js
+++ js/script.js
@@ -1,34 +1,45 @@
 'use strict';
 
 /*------Variaveis Gerais------*/
+// Guarda o tipo de moeda a ser utilizada no momento
 var coin_type = '€';
+// Guarda a pagina a ser disponibilizada no momento no index
 var current_page = 1;
+// Guarda o tipo de informação a ser procurada das moedas
 var current_country = 'eur'
-var original_link_section = $('.link_section').clone();
-var path_coingecko = "https://www.coingecko.com/en/coins/";
+
 var fav_list = new Array();
 //fav_list = JSON.parse(localStorage.getItem('fav_list'));
 
 /*------Variaveis API------*/
+
+// URL principal da API
 var API_url = 'https://api.coingecko.com/api/v3/';
-var serach_coins = 'search?query=';
 
 /*------Variaveis HTML------*/
+
+// Guarda o nome da página que este atualmente
 var page_name = window.location.pathname.split('/').slice(-1);
+// Guarda a estrutura principal onde vão ser inseridos os dados
 var cloneMedia = $('.media').clone();
+// Guarda a estrutura principal onde vão ser inseridos os dados das moedas favoritas
 var linha_fav = $('.fav_line').clone();
+// Guarda a estrutura principal onde vão ser os links nos detalhes
+var original_link_section = $('.link_section').clone();
 
 /*------Função on load do página------*/
 $(window).on('load', function () {
 	reload_fav_list();
 	if (page_name == 'index.html') {
+		// Carrega os dados no index
 		load_index();
-		$('.media').first().remove();
 	} else if (page_name == 'favoritos.html') {
 
 	} else if (page_name == 'search.html') {
+		// Carrega os dados de pesquisa
 		load_search();
 	} else if (page_name == 'detalhes.html') {
+		// Carrega os dados dos detalhes
 		load_details();
 	}
 });
@@ -45,7 +56,9 @@ $('#btSearch').on('click', function () {
 	}
 })
 
-//-Sistema de troca de página de criptomoedas-----//
+/*------Sistema de troca de página de criptomoedas------*/
+
+//Carrega a primeira página de moedas
 $('.first_page').on('click', function () {
 	$('.first_page').text('');
 	$('.current_page').text('Pág.1');
@@ -55,6 +68,7 @@ $('.first_page').on('click', function () {
 	load_index();
 })
 
+//Carrega a página de moedas anterior
 $('.back_page').on('click', function () {
 	if (current_page > 1) {
 		$('.current_page').text('Pág.' + --current_page);
@@ -67,6 +81,7 @@ $('.back_page').on('click', function () {
 	}
 })
 
+//Carrega a proxima página de moedas
 $('.next_page').on('click', function () {
 	if (current_page < 2) {
 		$('.first_page').text('«');
@@ -76,8 +91,8 @@ $('.next_page').on('click', function () {
 	removeAllChildNodes(document.querySelector('.media-list'));
 	load_index();
 })
-//---------------------------------------------------//
 
+/*------Test Area------*/
 $('#btTest').on('click', function () {
 	console.log('Im trying to load fav_list');
 	removeAllChildNodes(document.querySelector('.fav_list'));
@@ -142,23 +157,28 @@ function load_index() {
 			$('.rank', liMedia).text('#' + result.market_cap_rank);
 			$('.rank', liMedia).prop('title', 'Rank da moeda #' + result.market_cap_rank);
 			$('.detalhes', liMedia).prop('id', result.id);
-			//Guarda localmente a id da moeda selecionada para carregar os detalhes mais tarde
+			// Guarda localmente a id da moeda selecionada para carregar os detalhes mais tarde 
+			// e redireciona para á página de detalhes
 			$('.detalhes').on('click', function () {
 				console.log('Im trying to get into Coin ' + this.id + 'details');
 				var id_coin = this.id;
 				localStorage.setItem('coin_detail', id_coin);
 				window.location.href = 'detalhes.html'
 			})
+			// Carrega a imagem da moeda
 			$('#image', liMedia).attr('src', result.image);
 			$('#image', liMedia).prop('title', 'Logo ' + result.name);
+			// Carrega os dados do nome da moeda
 			$('.nome', liMedia).text(result.name);
 			$('.nome', liMedia).prop('title', 'Nome da moeda: ' + result.name);
+			// Carrega os dados do valor da moeda
 			$('.valor', liMedia).text(coin_type + result.current_price);
 			$('.valor', liMedia).prop('title', 'Valor atual: ' + coin_type + result.current_price);
 			var price_change_24h = result.price_change_24h;
 			$('.change24h', liMedia).text((price_change_24h > 0 ? '↑ ' : '↓ ') + price_change_24h + coin_type);
 			$('.change24h', liMedia).addClass(price_change_24h > 0 ? 'up' : 'down');
 			$('.change24h', liMedia).prop('title', 'Valor de ' + (price_change_24h > 0 ? 'subida ' : 'descida ') + 'da moeda');
+			// Procesa se a moeda esta como favorita ou não
 			$('#fav_button', liMedia).checked = fav_list.includes(result);
 			$('#fav_button', liMedia).change(function () {
 				if (this.checked) {
@@ -174,18 +194,23 @@ function load_index() {
 			// Adicionar o clone à tabela original
 			$('.media-list').append(liMedia);
 		})
+		//Remove o objeto original da página
+		$('.media').first().remove();
 	})
 }
 
 //Função de processo de dados da página de detalhes
 function load_details() {
+	if(!localStorage.getItem('coin_detail')){
+		location.document = 'index.html';
+	}
 	$.ajax({
 		method: 'GET',
 		url: API_url + 'coins/' + localStorage.getItem('coin_detail')
 	}).done(function (res) {
 		console.log(res);
 		//Datos imagem
-		$('#image a').attr('href', path_coingecko + res.id);;
+		$('#image a').attr('href', API_url + res.id);;
 		$('#image img').attr('src', res.image.large);
 		$('#image img').attr('alt', 'Logo ' + res.name);
 		$('#image img').attr('title', 'Logo ' + res.name + " & Link para a coingecko");
@@ -219,40 +244,40 @@ function load_details() {
 		$('#change_price_24h').text(value_state(0,valor) + coin_type);
 		$('#change_price_24h').addClass(value_state(1,valor));
 		$('#change_price_24h').prop('title', 'Valor ' + value_state(2,valor) + 'da moeda nas ultimas 24h');
-		//Data mudanza porcentagem em 24h
+		//Data mudanza Percentagem em 24h
 		valor = res.market_data.price_change_percentage_24h;
 		$('#change_por_24h').text(value_state(0,valor) + ' %');
 		$('#change_por_24h').addClass(value_state(1,valor));
-		$('#change_por_24h').prop('title', 'Porcentagem ' + value_state(2,valor) + 'da moeda nas ultimas 24h');
-		//Data mudanza porcentagem em 7d
+		$('#change_por_24h').prop('title', 'Percentagem ' + value_state(2,valor) + 'da moeda nas ultimas 24h');
+		//Data mudanza Percentagem em 7d
 		valor = res.market_data.price_change_percentage_7d;
 		$('#change_por_7d').text(value_state(0,valor) + ' %');
 		$('#change_por_7d').addClass(value_state(1,valor));
-		$('#change_por_7d').prop('title', 'Porcentagem ' + value_state(2,valor) + 'da moeda nod ultimos 7d');
-		//Data mudanza porcentagem em 14d
+		$('#change_por_7d').prop('title', 'Percentagem ' + value_state(2,valor) + 'da moeda nod ultimos 7d');
+		//Data mudanza Percentagem em 14d
 		valor = res.market_data.price_change_percentage_14d;
 		$('#change_por_14d').text(value_state(0,valor) + ' %');
 		$('#change_por_14d').addClass(value_state(1,valor));
-		$('#change_por_14d').prop('title', 'Porcentagem ' + value_state(2,valor) + 'da moeda nod ultimos 14d');
-		//Data mudanza porcentagem em 30d
+		$('#change_por_14d').prop('title', 'Percentagem ' + value_state(2,valor) + 'da moeda nod ultimos 14d');
+		//Data mudanza Percentagem em 30d
 		valor = res.market_data.price_change_percentage_30d;
 		$('#change_por_30d').text(value_state(0,valor) + ' %');
 		$('#change_por_30d').addClass(value_state(1,valor));
-		$('#change_por_30d').prop('title', 'Porcentagem ' + value_state(2,valor) + 'da moeda nod ultimos 30d');
-		//Data mudanza porcentagem em 60d
+		$('#change_por_30d').prop('title', 'Percentagem ' + value_state(2,valor) + 'da moeda nod ultimos 30d');
+		//Data mudanza Percentagem em 60d
 		valor = res.market_data.price_change_percentage_60d;
 		$('#change_por_60d').text(value_state(0,valor) + ' %');
 		$('#change_por_60d').addClass(value_state(1,valor));
-		$('#change_por_60d').prop('title', 'Porcentagem ' + value_state(2,valor) + 'da moeda nod ultimos 60d');
-		//Data mudanza porcentagem em 200d
+		$('#change_por_60d').prop('title', 'Percentagem ' + value_state(2,valor) + 'da moeda nod ultimos 60d');
+		//Data mudanza Percentagem em 200d
 		valor = res.market_data.price_change_percentage_200d;
 		$('#change_por_200d').text(value_state(0,valor) + ' %');
 		$('#change_por_200d').addClass(value_state(1,valor));
-		$('#change_por_200d').prop('title', 'Porcentagem ' + value_state(2,valor) + 'da moeda nod ultimos 200d');
-		//Data mudanza porcentagem em 200d
+		$('#change_por_200d').prop('title', 'Percentagem ' + value_state(2,valor) + 'da moeda nod ultimos 200d');
+		//Data mudanza Percentagem em 200d
 		valor = res.market_data.total_supply;
 		$('#total_supply').text(valor);
-		//Data mudanza porcentagem em 200d
+		//Data mudanza Percentagem em 200d
 		valor = res.market_data.max_supply;
 		$('#max_supply').text(valor);
 		
@@ -261,9 +286,12 @@ function load_details() {
 		//Data inserir links da moeda
 		$.each(res.links, function (index, link_list) {
 			var section = original_link_section.clone();
+			// Procesamento de titulos
 			index = index.charAt(0).toUpperCase() + index.slice(1);
 			index = index.replace('_url', '').replace('_', ' ');
 			$('.title_link', section).text(index);
+			// Caso for um array de dados ira inserir cada link
+			// Caso contrario, ira so inserir uma string
 			if (Array.isArray(link_list)) {
 				var original_text_area = $('.link_text', section).clone();
 				$.each(link_list, function (index, url) {
@@ -271,6 +299,7 @@ function load_details() {
 						var link_text = index == 0 ? $('.link_text', section) : original_text_area.clone();
 						$('a', link_text).attr('href', url);
 						$('a', link_text).text(url);
+						$('a', link_text).prop('title', 'Link para ' + url);
 						$('ul', section).append(link_text);
 					}
 				})
@@ -278,6 +307,7 @@ function load_details() {
 				var link_text = $('.link_text', section);
 				if (link_list.includes('http')) {
 					$('a', link_text).attr('href', link_list);
+					$('a', link_text).prop('title', 'Link para ' + link_list);
 				}
 				$('a', link_text).text(link_list);
 
@@ -292,6 +322,10 @@ function load_details() {
 
 //Função de processo de dados de pesquisa
 function load_search() {
+	// Caso não encontrar dados de pesquisa, voltar a página principal
+	if(!localStorage.getItem('coin_search')){
+		location.document = 'index.html';
+	}
 	$.ajax({
 		method: 'GET',
 		url: API_url + 'search?query=' + localStorage.getItem('coin_search')
