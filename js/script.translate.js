42c7cd13b2f54c7ba6e3500de77de126fc7f1ab5
David Machado Monteiro <2211849@my.ipleiria.pt>
2022-01-28 11:49:15 +0000

    PT & EN Translation
    - Generalização de tradução
    - Tradução de Footer & Heather a PT e EN

--- js/script.translate.js
+++ js/script.translate.js
@@ -10,134 +10,149 @@ $(window).on('load', function () {
     // Vai escolher o tipo de tradução a ser efetuada
     switch (idioma) {
         case 'PT':
-            // Tradução do heather
-            translate_pt_heather();
-            // A página a traducir
-            switch (page_name) {
-                case 'index.html':
-                    translate_pt_index();
-                    break;
-                case 'search.html':
-                    translate_pt_search();
-                    break;
-                case 'favoritos.html':
-                    translate_pt_fav();
-                    break;
-                case 'detalhes.html':
-                    translate_pt_detalhes();
-                    break;
-                default:
-                    console.log("Página não encontrada: " + page_name)
-                    break;
-            }
-            // Tradução footer
-            translate_pt_footer();
+            translate_pt();
             break;
         case 'EN':
-            console.log('English traduction');
-            // A página a traducir
-            switch (page_name) {
-                case 'index.html':
-                    translate_en_index();
-                    break;
-                case 'search.html':
-                    translate_en_search();
-                    break;
-                case 'favoritos.html':
-                    translate_en_fav();
-                    break;
-                case 'detalhes.html':
-                    translate_en_detalhes();
-                    break;
-                default:
-                    console.log("Page not found: " + page_name);
-                    break;
-            }
+            translate_en();
             break;
-        case 'ES':            
-            console.log('Traducción al español');
-            // A página a traducir
-            switch (page_name) {
-                case 'index.html':
-                    translate_es_index();
-                    break;
-                case 'search.html':
-                    translate_es_search();
-                    break;
-                case 'favoritos.html':
-                    translate_es_fav();
-                    break;
-                case 'detalhes.html':
-                    translate_es_detalhes();
-                    break;
-                default:
-                    console.log("Página no encontrada: " + page_name)
-                    break;
-            }
+        case 'ES':
+
             break;
-        // Caso existir um idioma fora dos parametros ira aparecer em ingles por default
+        // Caso existir um idioma fora dos parametros ira aparecer em ingles by default
         default:
-            // A página a traducir
-            switch (page_name) {
-                case 'index.html':
-                    translate_en_index();
-                    break;
-                case 'search.html':
-                    translate_en_search();
-                    break;
-                case 'favoritos.html':
-                    translate_en_fav();
-                    break;
-                case 'detalhes.html':
-                    translate_en_detalhes();
-                    break;
-                default:
-                    console.log("Page not found")
-                    break;
-            }
+            translate_en();
             break;
     }
 })
 
-/*------Translate to Portugues------*/
-
-function translate_pt_heather(){
-    $('#pesquisa').attr('placeholder','Procurar...');
-    $('#pesquisa').prop('title','Campo para inserir o nome de uma crypto moeda que quere ver');
-    $('#btSearch').prop('title','Procurar informação inserida');
-    $('#btHome').prop('title','Página principal');
-    $('#btFav').prop('title','Página de favoritos');
-    $('#dropdownMenuButton1').prop('title','Selecione idioma');
-    $('#dropdownMenuButton2').prop('title','Selecione Moeda');
+// Tradução da página a portugues
+function translate_pt() {
+    // Tradução do heather
+    translate_pt_heather();
+    // A página a traducir
+    switch (page_name) {
+        case 'index.html':
+            translate_pt_index();
+            break;
+        case 'search.html':
+            translate_pt_search();
+            break;
+        case 'favoritos.html':
+            translate_pt_fav();
+            break;
+        case 'detalhes.html':
+            translate_pt_detalhes();
+            break;
+        default:
+            console.log("Página não encontrada: " + page_name)
+            break;
+    }
+    // Tradução footer
+    translate_pt_footer();
 }
 
+// Tradução da página a ingles
+function translate_en() {
+    console.log('English traduction');
+    translate_en_heather();
+    // A página a traducir
+    switch (page_name) {
+        case 'index.html':
+            translate_en_index();
+            break;
+        case 'search.html':
+            translate_en_search();
+            break;
+        case 'favoritos.html':
+            translate_en_fav();
+            break;
+        case 'detalhes.html':
+            translate_en_detalhes();
+            break;
+        default:
+            console.log("Page not found: " + page_name);
+            break;
+    }
+    translate_en_footer();
+}
+
+// Tradução da página a espanhol
+function translate_es() {
+    console.log('Traducción al español');
+    // A página a traducir
+    switch (page_name) {
+        case 'index.html':
+            translate_es_index();
+            break;
+        case 'search.html':
+            translate_es_search();
+            break;
+        case 'favoritos.html':
+            translate_es_fav();
+            break;
+        case 'detalhes.html':
+            translate_es_detalhes();
+            break;
+        default:
+            console.log("Página no encontrada: " + page_name)
+            break;
+    }
+}
+/*------Translate to Portugues------*/
+
+// Tradução do heather a portugues
+function translate_pt_heather() {
+    $('#pesquisa').attr('placeholder', 'Procurar...');
+    $('#pesquisa').prop('title', 'Campo para inserir o nome de uma crypto moeda que quere ver');
+    $('#btSearch').prop('title', 'Procurar informação inserida');
+    $('#btHome').prop('title', 'Página principal');
+    $('#btFav').prop('title', 'Página de favoritos');
+    $('#dropdownMenuButton1').prop('title', 'Selecione idioma');
+    $('#dropdownMenuButton2').prop('title', 'Selecione Moeda');
+}
+
+// Tradução da página index a portugues
 function translate_pt_index() {
     console.log('Traducindo index');
 }
 
+// Tradução da página detalhes a portugues
 function translate_pt_detalhes() {
 
 }
 
+// Tradução da página favoritos a portugues
 function translate_pt_fav() {
 
 }
 
+// Tradução da página de pesquisa a portugues
 function translate_pt_search() {
 
 }
 
-function translate_pt_footer(){
-   $('.footer #info').text('CryptoCheck é um site que tem a resposabilidade de expor as moedas do mercado do mundo crypto. A nossa principal missão é fazer é dar capacitando os utilizadores de ver a variação das moedas em tempo real, para tirar as suas próprias conclusões informativa e pesquisar informação das mesmas.'); 
-   $('.footer #index_link').prop('title','Link a página principal');
-   $('.footer #fav_link').prop('title','Link a página de favoritos');
-   $('.footer #contact_names').text('Autores:');
-   $('.footer #contact_mails').text('Contactos:');
+// Tradução do footer a portugues
+function translate_pt_footer() {
+    $('.footer #info').text('CryptoCheck é um site que tem a resposabilidade de expor as moedas do mercado do mundo crypto. A nossa principal missão é fazer é dar capacitando os utilizadores de ver a variação das moedas em tempo real, para tirar as suas próprias conclusões informativa e pesquisar informação das mesmas.');
+    $('.footer #index_link').prop('title', 'Link a página principal');
+    $('.footer #fav_link').prop('title', 'Link a página de favoritos');
+    $('.footer #contact_names').text('Autores:');
+    $('.footer #contact_mails').text('Contactos:');
 
 }
 
 
 /*------Translate to English------*/
+
+function translate_en_heather() {
+    $('#pesquisa').attr('placeholder', 'Search...');
+    $('#pesquisa').prop('title', 'Campo para inserir o nome de uma crypto moeda que quere ver');
+    $('#btSearch').prop('title', 'Search info');
+    $('#btHome').prop('title', 'Home page');
+    $('#btFav').prop('title', 'Favorites page');
+    $('#dropdownMenuButton1').prop('title', 'Select a language');
+    $('#dropdownMenuButton2').prop('title', 'Select coin type');
+}
 
 function translate_en_index() {
 
@@ -152,6 +167,15 @@ function translate_en_fav() {
 }
 
 function translate_en_search() {
+
+}
+
+function translate_en_footer() {
+    $('.footer #info').text('CryptoCheck its a website with the responsability to show the actual coins in the market. Our principal mision its to having the capacity to users the variation of crypto coins in real time, letting the user taking their own conclusions e search about coins info.');
+    $('.footer #index_link').prop('title', 'Link home page');
+    $('.footer #fav_link').prop('title', 'Link favorites page');
+    $('.footer #contact_names').text('Creators:');
+    $('.footer #contact_mails').text('Contacts:');
 
 }
 
